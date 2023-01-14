df1e771c111682df65a549d871a0daa4faa22d60
Thomas Miller <thomas.miller147@gmail.com>
2023-01-14 22:47:37 +0100

    :heavy_plus_sign: tailwind, postcss nesting, gsap, netlify

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import { gsap } from 'gsap';
+	import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+	onMount(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    // testing gsap
+		gsap.to('.test', {
+			y: 100,
+			duration: 1
+		});
+    gsap.to('p', {
+    	scrollTrigger: {
+    		trigger: '.text-red-500',
+    		start: 'top 80%',
+    		end: 'bottom 20%',
+    		scrub: true
+    	},
+    	x: 100,
+    	y: 100,
+    	rotate: 360,
+    	scale: 2,
+    	duration: 1
+    });
+	});
+</script>
+
+<div class="h-[2000px]">
+	<h1 class="test text-red-500">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
+import nesting from 'postcss-nesting';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	// preprocess: vitePreprocess()
+	preprocess: [
+		preprocess({
+			postcss: {
+				plugins: [nesting()]
+			}
+		})
+	]
 };
 
 export default config;
