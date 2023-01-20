cdff744c132a2b80e479cd608cd87cced8e39a77
Thomas Miller <thomas.miller147@gmail.com>
2023-01-20 20:03:45 +0100

    :construction: temporary form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,31 @@
 <script>
 	import Nav from '$lib/components/Nav.svelte';
 	import LeftArrow from '$lib/icons/LeftArrow.svelte';
+
+	let name = '';
+	let email = '';
+	let telephone = '';
+	let message = '';
+	let errors = {};
+	function validate() {
+		errors = {};
+		if (!name) {
+			errors.name = 'Name is required';
+		}
+		if (!email) {
+			errors.email = 'Email is required';
+		} else if (!/^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/.test(email)) {
+			errors.email = 'Email is invalid';
+		}
+		if (!telephone) {
+			errors.telephone = 'Telephone is required';
+		} else if (!/^\\+[0-9]{2} [0-9]{2} [0-9]{3} [0-9]{2} [0-9]{2}$/.test(telephone)) {
+			errors.telephone = 'Telephone is invalid';
+		}
+		if (!message) {
+			errors.message = 'Message is required';
+		}
+	}
 </script>
 
 <svelte:head>
@@ -18,7 +43,65 @@
 		<h1>Contact</h1>
 	</div>
 
-	<div class="container">J'ai besoin d'amis, contact pls</div>
+	<div class="container">
+		<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" on:submit|preventDefault={validate}>
+			<div class="mb-4">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="name"> Name </label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					id="name"
+					name="name"
+					type="text"
+					placeholder="Your name.."
+					bind:value={name}
+				/>
+				{#if errors.name}
+					<p class="text-red-500 text-xs italic">{errors.name}</p>
+				{/if}
+			</div>
+			<div class="mb-4">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="email"> Email </label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					id="email"
+					name="email"
+					type="text"
+					placeholder="Your email.."
+					bind:value={email}
+				/>
+				{#if errors.email}
+					<p class="text-red-500 text-xs italic">{errors.email}</p>
+				{/if}
+			</div>
+			<div class="mb-4">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="telephone"> Telephone </label>
+				<input
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					id="telephone"
+					name="telephone"
+					type="text"
+					placeholder="Your telephone.."
+					bind:value={telephone}
+				/>
+				{#if errors.telephone}
+					<p class="text-red-500 text-xs italic">{errors.telephone}</p>
+				{/if}
+			</div>
+			<div class="mb-4">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="message"> Message </label>
+				<textarea
+					class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					id="message"
+					name="message"
+					placeholder="Write something.."
+					bind:value={message}
+				/>
+				{#if errors.message}
+					<p class="text-red-500 text-xs italic">{errors.message}</p>
+				{/if}
+			</div>
+		</form>
+	</div>
 
 	<div class="container">
 		<Nav />
